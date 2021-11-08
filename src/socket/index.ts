6b3ce3203ab92efc2098f31e0e5a126409f9f58b
Nathan TIEN YOU <nathan.tienyou@viacesi.fr>
2021-11-08 21:52:30 +0100

    fix keepalive

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,53 +1,47 @@
 class DumbWebSocket extends WebSocket {
+  pingTimeout: number = 0;
   constructor(url: string) {
     super(url);
-    this.addEventListener("message", (event) => {
+    this.onmessage = (event) => {
+      console.log("Message from server ", JSON.parse(event.data));
       const data = JSON.parse(event.data);
       if (!(<any>this)[data.order]) {
         console.error("unknown order : " + data.order);
         return;
       }
       (<any>this)[data.order](data);
-    });
+    };
+
+    this.onopen = (event) => {
+      this.heartbeat();
+    };
   }
-  ping(data: any) {
+
+  heartbeat(this: any) {
+    clearTimeout(this.pingTimeout);
+
+    this.pingTimeout = setTimeout(() => {
+      this.close();
+      console.warn("Connection timed out");
+    }, 30000 + 1000);
+  }
+
+  ping() {
     this.send("pong");
+    this.heartbeat();
   }
   getKnownUsersSharingIp(data: any) {}
 }
 
 let socket: DumbWebSocket;
 
-let pingTimeout: number;
-function heartbeat(this: any) {
-  clearTimeout(pingTimeout);
-
-  // Use `WebSocket#terminate()`, which immediately destroys the connection,
-  // instead of `WebSocket#close()`, which waits for the close timer.
-  // Delay should be equal to the interval at which your server
-  // sends out pings plus a conservative assumption of the latency.
-  pingTimeout = setTimeout(() => {
-    this.terminate();
-    console.warn("Connection timed out");
-  }, 30000 + 1000);
-}
-
 export function wsConnect(url: string) {
   return new Promise((resolve, reject) => {
     socket = new DumbWebSocket("ws://" + url);
-    socket.onopen = function (event) {
-      heartbeat.bind(socket)();
-      resolve(event);
-    };
-
-    // Listen for messages
-    socket.addEventListener("message", function (event) {
-      console.log("Message from server ", JSON.parse(event.data));
-    });
-
     socket.addEventListener("error", function (event) {
       reject(event);
     });
+    resolve(socket);
   });
 }
 
